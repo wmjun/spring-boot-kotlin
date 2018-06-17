d06af642b8166fff639a57c5b88438e26c98e359
dean.wm <dean.wm@kakaocorp.com>
2018-06-18 00:23:35 +0900

    handlerbars, logback

--- src/main/kotlin/com/dean/MainBootApplication.kt
+++ src/main/kotlin/com/dean/MainBootApplication.kt
@@ -3,9 +3,12 @@ package com.dean
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
+
 fun main(args: Array<String>) {
+
     SpringApplication.run(MainBootApplication::class.java, *args)
 }
+
 
 @SpringBootApplication
 class MainBootApplication {
--- src/main/kotlin/com/dean/controller/TickController.kt
+++ src/main/kotlin/com/dean/controller/TickController.kt
@@ -1,15 +1,20 @@
 package com.dean.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TickController {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @GetMapping("/tick", produces = [MediaType.APPLICATION_JSON_UTF8_VALUE])
-    fun tick(): String {
-        return "tick!"
+    fun tick(@RequestParam(defaultValue = "default") name: String): String {
+        logger.info("##### tick! $name")
+        return "tick! $name"
     }
 
 }
